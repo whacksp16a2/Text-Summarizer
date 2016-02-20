b5ea02c51173b8d30c53101f421bb0431db3a6e3
Joboman555 <Jonahspear512@gmail.com>
2016-02-20 18:48:57 -0500

    Added Pirate Speak Module

--- summarizer.py
+++ summarizer.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division, print_function, unicode_literals
 
 from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
@@ -10,11 +11,7 @@ from sumy.utils import get_stop_words
 LANGUAGE = "english"
 SENTENCES_COUNT = 2
 
-
-def getSummaryFromWebsite(url, sentences_count):
-
-    parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
-
+def getSummary(parser, sentences_count):
     stemmer = Stemmer(LANGUAGE)
 
     summarizer = Summarizer(stemmer)
@@ -26,7 +23,19 @@ def getSummaryFromWebsite(url, sentences_count):
 
     return sentences
 
+def getSummaryFromWebsite(url, sentences_count):
+
+    parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+
+    return getSummary(parser, sentences_count)
+
+def getSummaryFromFile(path, sentences_count):
+
+    parser = PlaintextParser.from_file(path, Tokenizer(LANGUAGE))
+
+    return getSummary(parser, sentences_count)
+
 if __name__ == "__main__":
-    url = "http://www.sparknotes.com/lit/frankenstein/summary.html"
-    sentences = getSummaryFromWebsite(url, SENTENCES_COUNT)
+    path = "BookThief.txt"
+    sentences = getSummaryFromFile(path, SENTENCES_COUNT)
     map(print, sentences)
