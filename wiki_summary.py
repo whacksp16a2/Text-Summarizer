360b76af49f9dae28e0db257460ad4b20402f12f
Joboman555 <Jonahspear512@gmail.com>
2016-02-20 20:07:19 -0500

    Added master Summarizer

--- wiki_summary.py
+++ wiki_summary.py
@@ -1,10 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib2, sys
 
-book_title = str(raw_input("What's your favorite book?"))
-
-
-
 
 def wiki_summary(title):
 	title = title.replace(" ", "_")
@@ -16,20 +12,16 @@ def wiki_summary(title):
 	req = urllib2.Request(site,headers=hdr)
 	page = urllib2.urlopen(req)
 
-
-	title = "The Book Thief"
 	soup = BeautifulSoup(page, 'html.parser')
 	plot_location_1 = str(soup.find(id="Plot"))
 	plot_location_2 = str(soup.find(id="Plot_summary"))
 
 	body = str(soup.body)
-	plot = "Plot"
 	if plot_location_1 in body:
 		index = body.index(plot_location_1)
 	else:
 		index = body.index(plot_location_2)
 
-	
 	plot_loc = body[index:]
 	index_para_start = plot_loc.index("<p>")
 	index_para_end = plot_loc.index("<h2>")
@@ -38,10 +30,6 @@ def wiki_summary(title):
 
 	return unstripped
 
-	para_start = 
-	para_end = 
-
-		#stripped = unstripped.strip('<p>')
-		# print stripped 
-
-print wiki_summary(book_title)
+if __name__ == "__main__":
+	book_title = str(raw_input("What's your favorite book?"))
+	print wiki_summary(book_title)
