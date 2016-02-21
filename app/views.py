7c205ac2a37680ef0067bbf1c2f15b891df0db86
Alexander Hoppe <alexander.hoppe@students.olin.edu>
2016-02-20 21:02:47 -0500

    fixed jinjaization

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', output_text='...')
+    return render_template('index.html', output_text='text')
 
 @app.route('/dev')
 def dev():
